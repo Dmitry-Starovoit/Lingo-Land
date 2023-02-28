bb51876fb7d8b4a0201b6bcdd9aaaa5f0046c694
Dmitry-Starovoit <dimastarovojt@gmail.com>
2023-02-28 13:52:18 +0200

    add list to a function in js file and scroll to content after click butto

--- script.js
+++ script.js
@@ -29,3 +29,42 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+// lists menu and other
+
+const footerListMenu = document.querySelector(".nav__footer");
+const headerListMenu = document.querySelector(".nav");
+const footerFollowList = document.querySelector(".follow__list");
+const listMenuItems = ["About us", "Resources", "News", "Contacts", "Feedback"];
+const listFollowItems = ["Facebook", "Instagram", "Telegram", "WhatsApp"];
+
+const listMenu = function () {
+  footerListMenu.innerHTML = "";
+  headerListMenu.innerHTML = "";
+  footerFollowList.innerHTML = "";
+
+  listMenuItems.reverse().forEach(function (value, key) {
+    const listsFooter = `<li class="nav__footer--item"><a href="">${value}</a></li>`;
+
+    const listsHeader = `<li class="nav__item"><a href="">${value}</a></li>`;
+
+    headerListMenu.insertAdjacentHTML("afterbegin", listsHeader);
+    footerListMenu.insertAdjacentHTML("afterbegin", listsFooter);
+  });
+
+  listFollowItems.reverse().forEach(function (value, key) {
+    const listFollow = `<li class="follow__list--item">
+    <a href=""><img src="images/${key + 1}.svg" alt="" />${value}</a>
+  </li>`;
+    footerFollowList.insertAdjacentHTML("afterbegin", listFollow);
+  });
+};
+listMenu();
+
+// scroll to block
+const scrollButton = document.querySelector(".show__more");
+const contentBlock = document.querySelector(".block__one--content");
+console.log(scrollButton);
+scrollButton.addEventListener("click", function () {
+  contentBlock.scrollIntoView({ behavior: "smooth" });
+});
